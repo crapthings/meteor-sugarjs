891440a1c517e62265b9be1295a9c0a5e2a491c3
crapthings <crapthings@gmail.com>
2013-08-28 13:50:20 +0800

    repack sugarjs, drop submodule, make it less pain.

--- package.js
+++ package.js
@@ -2,6 +2,7 @@ Package.describe({
   summary: "Sugar is a Javascript library that extends native objects with helpful methods. It is designed to be intuitive, unobtrusive, and let you do more with less code."
 });
 
-Package.on_use(function(api) {
-  api.add_files(['sugar/release/sugar-full.min.js'], ['client', 'server']);
+Package.on_use(function(api, where) {
+  where = where || ['client', 'server']
+  api.add_files(['sugar.js'], where);
 });
